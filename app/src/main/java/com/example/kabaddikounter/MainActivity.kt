d4bb92dcf349d896e4e852fb1db2ed538e9e72fe
YVESCREDIA <yves.ndeturuye@esisalama.org>
2023-07-11 14:33:47 +0200

    fin gestion team b data binding

--- app/src/main/java/com/example/kabaddikounter/MainActivity.kt
+++ app/src/main/java/com/example/kabaddikounter/MainActivity.kt
@@ -4,19 +4,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.kabaddikounter.databinding.ActivityMainBinding
 import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
 import com.example.kabaddikounter.databinding.ActivityMainBinding.inflate
 
 
 class MainActivity : AppCompatActivity() {
-    lateinit  var binding : ActivityMainBinding
+
     val viewModel: ScoreViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = inflate(this.layoutInflater)
-        setContentView(binding.root)
+        //changement du code pour utiliser le databinding
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
-        var scoreB = 0
+        binding.viewModel=viewModel
+        binding.lifecycleOwner=this
+
+        //mise à jour sans lifedata
         binding.btnPlus1TeamA.setOnClickListener(){
             viewModel.incrementA(true,1)
             binding.txtScoreTeamA.text = viewModel.scoreA.toString()
@@ -27,6 +31,12 @@ class MainActivity : AppCompatActivity() {
             binding.txtScoreTeamA.text = viewModel.scoreA.toString()
         }
 
-
+        //mise à jour avec lifedata
+        binding.btnPlus1TeamB.setOnClickListener(){
+            viewModel.incrementA(false,1)
+        }
+        binding.btnPlus2TeamB.setOnClickListener {
+            viewModel.incrementA(false,2)
+        }
     }
 }
